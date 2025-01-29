0048588c852361ec35c494ffac969cd82b0dec53
Kevin Calvo Brenes <kev170103@gmail.com>
2025-01-28 18:38:36 -0600

    Commit inicial del proyecto

---
+++ KN_ProyectoClase/Controllers/PersonasController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KN_ProyectoClase.Models;
+
+namespace KN_ProyectoClase.Controllers
+{
+    public class PersonasController : Controller
+    {
+        [HttpGet]
+        public ActionResult RegistrarPersona()
+        {
+            var personasModelo = new PersonasModel();
+            personasModelo.identificacion = "305430780";
+            personasModelo.nombre = "Kevin";
+
+            return View(personasModelo);
+        }
+
+        [HttpPost]
+        public ActionResult RegistrarPersona(PersonasModel modelo)
+        { 
+            return View();
+        }
+    }
+}
