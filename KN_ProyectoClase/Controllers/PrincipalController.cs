d1ee703278046b2b68a38a7772fc5cd304ca890c
Kevin Calvo Brenes <kev170103@gmail.com>
2025-02-04 19:18:58 -0600

    SEM03 receso

--- KN_ProyectoClase/Controllers/PrincipalController.cs
+++ KN_ProyectoClase/Controllers/PrincipalController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using KN_ProyectoClase.Models;
 
 namespace KN_ProyectoClase.Controllers
 {
@@ -22,6 +23,12 @@ namespace KN_ProyectoClase.Controllers
 
         [HttpGet]
         public ActionResult RegistrarCuenta() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegistrarCuenta(UsuarioModel model)
         {
             return View();
         }
